7625872d3189eed757a6ff586d937e1c3d6f4c86
ambodi <amir.rahnama@gmail.com>
2018-02-03 12:40:42 +0100

    loss visualization works (kinda) without the loop yet

--- loss_visualization.py
+++ loss_visualization.py
@@ -1,3 +1,4 @@
+"""2-d visualization of restricted loss."""
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -9,17 +10,25 @@ this.initial_w1 = np.random.uniform(0, 1, (3, 4))
 this.initial_w2 = np.random.uniform(0, 1, (4, 1))
 
 
-fig = plt.figure()
+fig, ax = plt.subplots()
 ax = fig.add_subplot(111)
 
 var = 1
-for k in range(3):
+alpha = 0.1
+
+t_ranges = np.linspace(-1, 1, num=100)
+line, = plt.plot(t_ranges)
+plt.ion()
+plt.show()
+
+
+for k in range(1, 100):
     new_weights = []
     random_w_1 = var*np.random.normal(size=(3, 4))
     random_w_2 = var*np.random.normal(size=(4, 1))
 
     scores = []
-    t_ranges = np.linspace(-1, 1, num=100)
+    # t_ranges = np.linspace(-1, 1, num=100)
 
     for t in t_ranges:
         new_w_1 = this.initial_w1 + t * random_w_1
@@ -27,9 +36,30 @@ for k in range(3):
 
         fp = mlp_xor.forward_pass(new_w_1, new_w_2)
         loss_value = mlp_xor.loss(fp, mlp_xor.y)
-        scores.append(loss_value)
+
+        this.scores.append(loss_value)
 
     ax.clear()
-    ax.plot(t_ranges, scores)
+    line.set_ydata(scores)
+    plt.draw()
+    time.sleep(0.1)
+    plt.pause(0.0001)
+    # ax.plot(t_ranges, scores)
 
+plt.show()
+
+
+def animate(scores):
+    line.set_ydata(scores)  # update the data
+    return line,
+
+
+# Init only required for blitting to give a clean slate.
+def init(scores):
+    line.set_ydata(scores)
+    return line,
+
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
+                              interval=25, blit=True)
 plt.show()
