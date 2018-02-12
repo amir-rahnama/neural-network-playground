new updated formula still has a bug

2d-visualization.py
@@ -24,13 +24,6 @@ this.a_3 = np.empty((4, 1))
 
 this.learning_rates = [0.1, 0.001, 10, 100]
 
-plt.ion()
-
-fig, ax = plt.subplots(2, 2)
-fig.set_size_inches(18.5, 10.5)
-
-mng = plt.get_current_fig_manager()
-
 
 def relu(x):
     """Calculate the RELU activation."""
@@ -106,13 +99,13 @@ def gd(learn_rates, epochs):
     this.alpha = []
     this.beta = []
 
-    this.w_1_t = []
-    this.w_1_t.append(this.w_1_rate_0)
-
-    this.w_2_t = []
-    this.w_2_t.append(this.w_2_rate_0)
-
     i = 0
+
+    this.w_1_t = np.zeros((epochs, 3, 4))
+    this.w_1_t[i] = this.w_1_rate_0
+
+    this.w_2_t = np.zeros((epochs, 4, 1))
+    this.w_2_t[i] = this.w_2_rate_0
 
     while i < epochs:
         this.fp_rate_0 = forward_pass(this.w_1_rate_0, this.w_2_rate_0)
@@ -120,11 +113,11 @@ def gd(learn_rates, epochs):
                                               this.w_2_rate_0)
 
         this.w_1_rate_0_new = learn_rates[0] * dw_1_rate_0
-        this.w_1_t.append(this.w_1_rate_0_new)
+        this.w_1_t[i] = this.w_1_rate_0_new
         this.w_1_rate_0 -= this.w_1_rate_0_new
 
         this.w_2_rate_0_new = learn_rates[0] * dw_2_rate_0
-        this.w_2_t.append(this.w_2_rate_0_new)
+        this.w_2_t[i] = this.w_2_rate_0_new
         this.w_2_rate_0 -= this.w_2_rate_0_new
 
         new_fp = forward_pass(this.w_1_rate_0, this.w_2_rate_0)
@@ -138,41 +131,41 @@ def gd(learn_rates, epochs):
     return [this.w_1_rate_0, this.w_2_rate_0]
 
 
-def plot(w_1, w_2):
+def plot(W_1, W_2):
     alpha = []
     beta = []
 
-    w_1_init = this.w_1_t[0]
-    w_2_init = this.w_2_t[0]
+    diff_u_w_1 = np.subtract(this.w_1_t, this.w_1_t[0])
+    diff_u_w_2 = np.subtract(this.w_2_t, this.w_2_t[0])
 
-    diff_u_w_1 = np.subtract(this.w_1_t, w_1_init)
-    diff_u_w_2 = np.subtract(this.w_2_t, w_2_init)
-
-    u_w_1_direction = np.subtract(this.w_1, w_1_init)
-    u_w_2_direction = np.subtract(this.w_2, w_2_init)
+    u_w_1_direction = np.subtract(W_1, this.w_1_t[0])
+    u_w_2_direction = np.subtract(W_2, this.w_2_t[0])
 
     u_w_1 = u_w_1_direction / LA.norm(u_w_1_direction)
     u_w_2 = u_w_2_direction / LA.norm(u_w_2_direction)
 
-    alpha_w_1 = np.dot(diff_u_w_1.T, u_w_1)
-    alpha_w_2 = np.dot(diff_u_w_2.T, u_w_2)
+    alpha_w_1 = np.einsum('ijk,km->ijm', np.swapaxes(diff_u_w_1, 1, 2), u_w_1)
+    alpha_w_2 = np.einsum('ijk,km->ijm', np.swapaxes(diff_u_w_2, 1, 2), u_w_2)
 
-    alpha.append(LA.norm(alpha_w_1) + LA.norm(alpha_w_2))
+    # alpha.append(LA.norm(alpha_w_1, axis=(1, 2)) + LA.norm(alpha_w_2, axis=(1, 2)))
+    alpha.append(LA.norm(alpha_w_2, axis=(1, 2)))
 
-    diff_v_w_1 = this.w_1_rate_0_new - this.w_1_rate_0_init - np.dot(u_w_1, alpha_w_1)
-    diff_v_w_2 = this.w_2_rate_0_new - this.w_2_rate_0_init - np.dot(u_w_2, alpha_w_2)
+    diff_v_w_1 = this.w_1_t - this.w_1_t[0] - np.einsum('km,ijm->ikm', u_w_1, alpha_w_1)
+    diff_v_w_2 = this.w_2_t - this.w_2_t[0] - np.einsum('km,ijm->ikm', u_w_2, alpha_w_2)
 
     v_w_1 = diff_v_w_1 / LA.norm(diff_v_w_1)
     v_w_2 = diff_v_w_2 / LA.norm(diff_v_w_2)
 
-    beta_w_1 = np.dot(diff_v_w_1.T, v_w_1)
-    beta_w_2 = np.dot(diff_v_w_2.T, v_w_2)
+    beta_w_1 = np.einsum('ijk,ikm->imj', diff_v_w_1, np.swapaxes(v_w_1, 1, 2))
+    beta_w_2 = np.einsum('ijk,ikm->imj', diff_v_w_2, np.swapaxes(v_w_2, 1, 2))
 
-    beta.append(LA.norm(beta_w_1) + LA.norm(beta_w_2))
+    beta.append(LA.norm(beta_w_1, axis=(1, 2)) + LA.norm(beta_w_2, axis=(1, 2)))
+    #beta.append(LA.norm(beta_w_1, axis=(1, 2)))
 
-    plt.scatter(alpha, beta)
+    plt.scatter(np.arange(0, 600), alpha)
+    plt.show()
 
 
 if __name__ == '__main__':
-    [w_1, w_2] = gd([10], epochs=10)
+    [w_1, w_2] = gd([10], epochs=600)
     plot(w_1, w_2)